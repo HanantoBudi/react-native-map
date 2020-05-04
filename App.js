501e197e54a522eac8869f702b3975f2f2bbdc4f
hananto <hananto.budi@imaniprima.co.id>
2020-05-04 15:59:16 +0700

    First Commit

---
+++ App.js
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import MapView from 'react-native-maps';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView style={styles.map}
+          initialRegion={{
+            latitude: -7.198417,
+            longitude: 112.733575,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position:'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  map: {
+    position:'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0
+  }
+});
